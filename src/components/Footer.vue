3c2901ac44aed0c535b896e93018b694e153569c
Lernimor <you@example.com>
2019-01-02 11:32:35 +0800

    aa

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,18 +1,40 @@
 <template>
      <div class="footer">
         <ul>
-            <li v-on:click="changeType($event)"><img src="../../public/img/main/home_unsel.png"></li>
-            <li v-on:click="changeType($event)"><img src="../../public/img/main/fenlei0101.png"></li>
-            <li v-on:click="changeType($event)"><img src="../../public/img/main/buycar_unsel.png"></li>
-            <li v-on:click="changeType($event)"><img src="../../public/img/main/my_unsel.png"></li>
+            <li v-on:click="changeType($event,1)">
+                <img :src="activeIndex == 1 ? 
+                        require('../../public/img/main/home_sel.png') : 
+                        require('../../public/img/main/home_unsel.png')">
+            </li>
+            <li v-on:click="changeType($event,2)">
+                <img :src="activeIndex == 2 ? 
+                        require('../../public/img/main/search_sel.png') : 
+                        require('../../public/img/main/search_unsel.png')">
+                </li>
+            <li v-on:click="changeType($event,3)">
+                <img :src="activeIndex == 3 ? 
+                        require('../../public/img/main/buycar_sel.png') : 
+                        require('../../public/img/main/buycar_unsel.png')">
+            </li>
+            <li v-on:click="changeType($event,4)">
+                <img :src="activeIndex == 4 ? 
+                        require('../../public/img/main/my_sel.png') : 
+                        require('../../public/img/main/my_unsel.png')">
+            </li>
         </ul>
     </div>
 </template>
 
 <script>
     export default {
+        data() {
+            return {
+                activeIndex : 1
+            }
+        },
         methods : {
-            changeType : function(event){
+            changeType : function(event, index){
+                this.activeIndex = index;
                 let liCom = event.currentTarget;
                 liCom.querySelector('img');
             }
