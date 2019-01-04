667db1d42e8daacd20a71796223febdd83e3b99b
Lernimor <lernimor@icloud.com>
2019-01-05 01:03:21 +0800

    s

--- src/components/GoodsFile/GoodsInfoPage.vue
+++ src/components/GoodsFile/GoodsInfoPage.vue
@@ -1,7 +1,7 @@
 <template>
 	<div class="goods_info_page">
 		<carousel-img></carousel-img>
-		<div class="goods_info">
+		<div class="goods_info show_box">
 			<div class="goods_name"><span>美国进口-莱泽曼26工具手链</span></div>
 			<div class="goods_otherdec">
 				<span class="old_price">原价:￥1330</span> <span class="discount">钜惠9折</span>
@@ -17,31 +17,41 @@
 				<div class="otherDesc">距离结束:1天23小时51分</div>
 			</div>
 		</div>
-		<div class="goods_tab">
-			<input type="radio" name="goods_tab" id="new" checked="checked" />
-			<input type="radio" name="goods_tab" id="hot" />
-			<input type="radio" name="goods_tab" id="random" />
+		<div class="empty"></div>
+		<div class="shop_info show_box">
+			<div class="shop_base_info">
+
+			</div>
+			<div class="shop_base_info"></div>
+		</div>
+		<div class="empty"></div>
+		<div class="goods_tab show_box">
+			<input type="radio" name="goods_tab" id="detail" checked="checked" />
+			<input type="radio" name="goods_tab" id="comment" />
 			<div class="widget-title">
 				<ul>
-					<li class="new">
-						<label for="new">最新博文</label>
+					<li class="detail">
+						<label for="detail">详情</label>
 					</li>
-					<li class="hot">
-						<label for="hot">热评博文</label>
-					</li>
-					<li class="random">
-						<label for="random">随机博文</label>
+					<li class="comment">
+						<label for="comment">评价</label>
 					</li>
 				</ul>
 			</div>
 			<div class="widget-box">
-				<ul class="new-list">
+				<ul class="detail-list">
+					<li>new</li>
+					<li>new</li>
+					<li>new</li>
+					<li>new</li>
+					<li>new</li>
+					<li>new</li>
+					<li>new</li>
+					<li>new</li>
+					<li>new</li>
 					<li>new</li>
 				</ul>
-				<ul class="hot-list">
-					<li>hot</li>
-				</ul>
-				<ul class="random-list">
+				<ul class="comment-list">
 					<li>random</li>
 				</ul>
 			</div>
@@ -59,18 +69,31 @@
 </script>
 
 <style>
+	.goods_info_page {
+		background: rgba(216, 216, 216, 0.253);
+	}
+
+	.empty {
+		height: 20px
+	}
+
+	.show_box {
+		box-shadow: 0 5px 5px rgba(153, 153, 155, 0.377);
+	}
+
 /*goods_info*/
+	.empty,
 	.goods_info,
 	.goods_name,
 	.goods_price,
-	.goods_otherdec {
+	.goods_otherdec,
+	.shop_info {
 		width: 100%;
 	}
 
 	.goods_info {
 		height: 80px;
-		background: #f5f5f5;
-		box-shadow: 0 0 10px rgb(153, 153, 155);
+		background: white;
 	}
 
 	.goods_name {
@@ -152,6 +175,13 @@
 		height: 100%;
 		vertical-align: top
 	}
+
+/*shop_info*/
+	.shop_info {
+		height: 150px;
+		background: white;
+	}
+
 /*goods_tab*/
 	.goods_tab>input[type="radio"] {
 		display: none
@@ -159,34 +189,42 @@
 
 	.goods_tab {
 		width: 100%;
-		padding-top: 5%;
 	}
 
 	.goods_tab .widget-box ul {
+		background: white;
 		display: none;
 		width: 100%;
 	}
 
-	.goods_tab .widget-title li{
+	.goods_tab .widget-title ul {
+		width: 100%;
+	}
+
+	.goods_tab .widget-title li {
 		list-style: none;
 		display: inline-block;
+		margin-left: 1%;
+		height: 36px;
+		line-height: 36px;
 		text-align: center;
-		width: 33.3%;
+		width: calc(48%);
+		background: white;
+		border: 1px solid rgba(47, 79, 79, 0.048);
+		border-top-left-radius: 5px;
+		border-top-right-radius: 5px;
+		border-bottom: 0px;
 	}
 
-	.goods_tab .widget-title label{
-	}
-	
-
-	#new:checked~.widget-title .new,
-	#hot:checked~.widget-title .hot,
-	#random:checked~.widget-title .random {
-		background: rgb(128, 127, 127)
+	#detail:checked~.widget-title .detail,
+	/* #hot:checked~.widget-title .hot, */
+	#comment:checked~.widget-title .comment {
+		box-shadow: darkgrey 0px -5px 5px;
 	}
 
-	#new:checked~.widget-box .new-list,
-	#hot:checked~.widget-box .hot-list,
-	#random:checked~.widget-box .random-list {
+	#detail:checked~.widget-box .detail-list,
+	/* #hot:checked~.widget-box .hot-list, */
+	#comment:checked~.widget-box .comment-list {
 		display: block
 	}
 </style>
--- src/components/Tools/CarouselImg.vue
+++ src/components/Tools/CarouselImg.vue
@@ -1,9 +1,6 @@
 <template>
     <div class="carouselContain">
-        <v-touch class="imgPenel animation" 
-            v-on:swipeleft="next_pic()" 
-            v-on:swiperight="prev_pic()"
-            :swipe-options="{direction: 'horizontal'}">
+        <v-touch class="imgPenel animation" v-on:swipeleft="next_pic()" v-on:swiperight="prev_pic()" :swipe-options="{direction: 'horizontal'}">
             <div class="imgsContaier" v-for="imgUrl in imgUrls" :key="imgUrl">
                 <img class="imgs" :src="imgUrl">
             </div>
@@ -11,9 +8,7 @@
         <div class="clear"></div>
         <div class="btns">
             <div v-for="(item,index) in imgUrls" :key="item">
-                <span class="to_picbtn" 
-                        :class="index == 0 ? 'on' : ''" 
-                        @click="to_pic(index)">
+                <span class="to_picbtn" :class="index == 0 ? 'on' : ''" @click="to_pic(index)">
                 </span>
             </div>
         </div>
@@ -23,163 +18,171 @@
 </template>
 
 <script>
-export default {
-    data(){
-        return {
-            imgUrls:[
-                require("../../../public/img/lins/lernimor_ns.jpg"),
-                require("../../../public/img/lins/lernimor.jpg"),
-            ],
-            imgWidth:0
-        }
-    },
-    mounted:function(){
-        let imgWidth = document.querySelector(".carouselContain").clientWidth;
-        document.querySelector(".imgPenel").style.width = parseInt(imgWidth * 2) +'px';
-        let iCs = document.querySelectorAll(".imgsContaier");
-        for (let i=0;i<this.imgUrls.length;i++){
-            iCs[i].style.width = parseInt(imgWidth) +'px';
-        }
-    },
-    methods:{
-        to_pic:function (index){
-            let wrap = document.querySelector(".imgPenel");
-            let w = document.querySelector(".carouselContain").clientWidth;
-            let newLeft = 0 - w * index;
-            if (newLeft + "px" == wrap.style.left 
-                    || (newLeft == 0 && !wrap.style.left)){
-                return;
+    export default {
+        data() {
+            return {
+                imgUrls: [
+                    require("../../../public/img/lins/lernimor_ns.jpg"),
+                    require("../../../public/img/lins/lernimor.jpg"),
+                ],
+                imgWidth: 0
             }
-            this.addTranListener(wrap);
-            wrap.style.left = newLeft + "px";
-            let btns = document.querySelectorAll('.to_picbtn');
-            for (let i=0; i<btns.length; i++){
-                btns[i].classList.remove('on');
-            }
-            btns[index].classList.add('on');
         },
-        next_pic:function () {
-            let wrap = document.querySelector(".imgPenel");
-            let w = document.querySelector(".carouselContain").clientWidth;
-            let newLeft = parseInt(wrap.offsetLeft) - w;
-            if (wrap.style.left == (0 - w * (this.imgUrls.length - 1) + "px")){
-                newLeft = 0;
+        mounted: function () {
+            let imgWidth = document.querySelector(".carouselContain").clientWidth;
+            document.querySelector(".imgPenel").style.width = parseInt(imgWidth * 2) + 'px';
+            let iCs = document.querySelectorAll(".imgsContaier");
+            for (let i = 0; i < this.imgUrls.length; i++) {
+                iCs[i].style.width = parseInt(imgWidth) + 'px';
             }
-            let index = newLeft / w;
-            this.to_pic(Math.abs(index));
         },
-        prev_pic:function() {
-            let wrap = document.querySelector(".imgPenel");
-            let w = document.querySelector(".carouselContain").clientWidth;
-            let newLeft = parseInt(wrap.offsetLeft) + w;
-            if (wrap.style.left == "0px" || wrap.style.left == ""){
-                newLeft = 0 - w * (this.imgUrls.length - 1);
-            }
-            let index = newLeft / w;
-            this.to_pic(Math.abs(index));
-        },
-        getTransition : function (){
-            let el = document.createElement('surface'),
-            transitions = {
-                'transition':'transitionend',
-                'OTransition':'oTransitionEnd',
-                'MozTransition':'transitionend',
-                'WebkitTransition':'webkitTransitionEnd'
-            }
-            for(let t in transitions){
-                if( el.style[t] !== undefined ){
-                    return transitions[t];
+        methods: {
+            to_pic: function (index) {
+                let wrap = document.querySelector(".imgPenel");
+                let w = document.querySelector(".carouselContain").clientWidth;
+                let newLeft = 0 - w * index;
+                if (newLeft + "px" == wrap.style.left ||
+                    (newLeft == 0 && !wrap.style.left)) {
+                    return;
                 }
-            }
-        },
-        addTranListener : function (el){
-            let btns = document.querySelectorAll('.to_picbtn');
-            for (let i=0; i<btns.length; i++){
-                btns[i].classList.add('cancelClick');
-            }
-            document.querySelector('.imgPenel').classList.add('cancelClick');
-            document.querySelector('.arrow_right').classList.add('cancelClick');
-            document.querySelector('.arrow_left').classList.add('cancelClick');
+                this.addTranListener(wrap);
+                wrap.style.left = newLeft + "px";
+                let btns = document.querySelectorAll('.to_picbtn');
+                for (let i = 0; i < btns.length; i++) {
+                    btns[i].classList.remove('on');
+                }
+                btns[index].classList.add('on');
+            },
+            next_pic: function () {
+                let wrap = document.querySelector(".imgPenel");
+                let w = document.querySelector(".carouselContain").clientWidth;
+                let newLeft = parseInt(wrap.offsetLeft) - w;
+                if (wrap.style.left == (0 - w * (this.imgUrls.length - 1) + "px")) {
+                    newLeft = 0;
+                }
+                let index = newLeft / w;
+                this.to_pic(Math.abs(index));
+            },
+            prev_pic: function () {
+                let wrap = document.querySelector(".imgPenel");
+                let w = document.querySelector(".carouselContain").clientWidth;
+                let newLeft = parseInt(wrap.offsetLeft) + w;
+                if (wrap.style.left == "0px" || wrap.style.left == "") {
+                    newLeft = 0 - w * (this.imgUrls.length - 1);
+                }
+                let index = newLeft / w;
+                this.to_pic(Math.abs(index));
+            },
+            getTransition: function () {
+                let el = document.createElement('surface'),
+                    transitions = {
+                        'transition': 'transitionend',
+                        'OTransition': 'oTransitionEnd',
+                        'MozTransition': 'transitionend',
+                        'WebkitTransition': 'webkitTransitionEnd'
+                    }
+                for (let t in transitions) {
+                    if (el.style[t] !== undefined) {
+                        return transitions[t];
+                    }
+                }
+            },
+            addTranListener: function (el) {
+                let btns = document.querySelectorAll('.to_picbtn');
+                for (let i = 0; i < btns.length; i++) {
+                    btns[i].classList.add('cancelClick');
+                }
+                document.querySelector('.imgPenel').classList.add('cancelClick');
+                document.querySelector('.arrow_right').classList.add('cancelClick');
+                document.querySelector('.arrow_left').classList.add('cancelClick');
 
-            let transition = this.getTransition();
-            let tranListener = function (){
-                for (let i=0; i<btns.length; i++){
-                    btns[i].classList.remove('cancelClick');
+                let transition = this.getTransition();
+                let tranListener = function () {
+                    for (let i = 0; i < btns.length; i++) {
+                        btns[i].classList.remove('cancelClick');
+                    }
+                    document.querySelector('.imgPenel').classList.remove('cancelClick');
+                    document.querySelector('.arrow_left').classList.remove('cancelClick');
+                    document.querySelector('.arrow_right').classList.remove('cancelClick');
+                    el.removeEventListener(transition, tranListener, false);
                 }
-                document.querySelector('.imgPenel').classList.remove('cancelClick');
-                document.querySelector('.arrow_left').classList.remove('cancelClick');
-                document.querySelector('.arrow_right').classList.remove('cancelClick');
-                el.removeEventListener(transition, tranListener, false);
+                el.addEventListener(transition, tranListener);
             }
-            el.addEventListener(transition, tranListener);
         }
     }
-}
 </script>
 
 <style>
-    a{
+    a {
         text-decoration: none;
     }
+
     .carouselContain {
         position: relative;
         width: 100%;
         height: 40%;
         overflow: hidden;
     }
-    .imgPenel{
+
+    .imgPenel {
         position: absolute;
-        left : 0px;
+        left: 0px;
         width: 100%;
-        height:100%;
+        height: 100%;
         z-index: 1;
     }
+
     .animation {
         transition: all 1s;
         animation: all 2s linear 1s 3 normal;
     }
+
     .imgPenel div {
         float: left;
         width: 100%;
-        height:100%;
+        height: 100%;
         background: rgba(77, 77, 77, 1);
         text-align: center;
         vertical-align: middle;
     }
+
     .imgPenel .imgs {
         width: 100%;
         height: auto;
-        transform: translate(0,0);
+        transform: translate(0, 0);
     }
 
     .btns {
         position: absolute;
         text-align: center;
-        bottom:20px;
+        bottom: 20px;
         width: 100%;
         height: 10px;
         z-index: 2;
     }
-    .btns div{
-         display: inline-block;
-         padding-left: 3px;
-         color:white;
+
+    .btns div {
+        display: inline-block;
+        padding-left: 3px;
+        color: white;
     }
-    .btns div span{
+
+    .btns div span {
         display: inline-block;
         width: 10px;
         height: 5px;
-        background: rgba(106, 170, 219,0.5);
+        background: rgba(106, 170, 219, 0.5);
         cursor: pointer;
     }
-    .btns div span.on{
+
+    .btns div span.on {
         background-color: rgb(252, 86, 86);
     }
 
-    .arrow{
+    .arrow {
         position: absolute;
         width: 40px;
-        height : 100%;
+        height: 100%;
         line-height: 550%;
         font-size: 34px;
         color: white;
@@ -188,41 +191,64 @@ export default {
         z-index: 2;
     }
 
-    .arrow{
+    .arrow {
         display: block;
     }
 
-    .arrow_left{
+    .arrow_left {
         left: 0;
     }
 
-    .arrow_right{
+    .arrow_right {
         right: 0;
     }
 
-    @keyframes myfirst
-    {
-        0% {opacity: 0;}
-        100% {opacity: 1;}
+    @keyframes myfirst {
+        0% {
+            opacity: 0;
+        }
+
+        100% {
+            opacity: 1;
+        }
     }
 
-    @-moz-keyframes myfirst /* Firefox */
-    {
-        0% {opacity: 0;}
-        100% {opacity: 1;}
+    @-moz-keyframes myfirst
+
+    /* Firefox */
+        {
+        0% {
+            opacity: 0;
+        }
+
+        100% {
+            opacity: 1;
+        }
     }
 
-    @-webkit-keyframes myfirst /* Safari 和 Chrome */
-    {
-        0% {opacity: 0;}
-        100%  {opacity: 1;}
+    @-webkit-keyframes myfirst
+
+    /* Safari 和 Chrome */
+        {
+        0% {
+            opacity: 0;
+        }
+
+        100% {
+            opacity: 1;
+        }
     }
 
-    @-o-keyframes myfirst /* Opera */
-    {
-        0% {opacity: 0;}
-        100%  {opacity: 1;}
-    }
+    @-o-keyframes myfirst
 
+    /* Opera */
+        {
+        0% {
+            opacity: 0;
+        }
+
+        100% {
+            opacity: 1;
+        }
+    }
 </style>
-
